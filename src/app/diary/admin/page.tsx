de31cc4daa002a919a6d660af207b6f8e926bb09
Haruna Fujishima <ringo144516@gmail.com>
2026-04-15 15:02:53 -0400

    added work

---
+++ src/app/diary/admin/page.tsx
@@ -0,0 +1,82 @@
+'use client'
+
+import Link from "next/link";
+import { useState } from 'react'
+import { Listbox, ListboxButton, ListboxOption, ListboxOptions } from '@headlessui/react'
+import { ChevronUpDownIcon } from '@heroicons/react/16/solid'
+import { CheckIcon } from '@heroicons/react/20/solid'
+
+const topics = [
+  { id: 1, des: "study" },
+  { id: 2, des: "HTML" },
+  { id: 3, des: "CSS" },
+  { id: 4, des: "Backend" },
+  { id: 5, des: "Full-stack" },
+  { id: 6, des: "Javascript" },
+  { id: 7, des: "React" },
+  { id: 8, des: "Next.js" },
+  { id: 9, des: "Develop" },
+  { id: 10, des: "life" }
+];
+
+export default function EditDiary() {
+    const [selected, setSelected] = useState([topics[0]])
+
+    return (
+        <main className="mx-auto w-full max-w-5xl px-8 py-16 sm:px-12">
+            <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-full border p-4">
+                <legend className="fieldset-legend">Diary Content</legend>
+
+                <label className="label">Title</label>
+                <input type="text" className="input w-full" placeholder="My awesome page" />
+
+                <label className="label">Content</label>
+                <textarea
+                className="textarea textarea-bordered w-full h-40"
+                placeholder="Write your diary here..."
+                ></textarea>
+
+                <Listbox value={selected} onChange={setSelected} multiple>
+                    <label className="label">Topic</label>
+                    <div className="relative mt-2">
+                        <ListboxButton className="grid w-full cursor-default grid-cols-1 rounded-md bg-white py-1.5 pr-2 pl-3 text-left text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus-visible:outline-2 focus-visible:-outline-offset-2 focus-visible:outline-indigo-600 sm:text-sm/6">
+                        <span className="col-start-1 row-start-1 flex items-center gap-3 pr-6">
+                            <span className="block truncate">
+                                {selected.length > 0
+                                    ? selected.map((topic) => topic.des).join(", ")
+                                    : "Select topics"}
+                            </span>
+                        </span>
+                        <ChevronUpDownIcon
+                            aria-hidden="true"
+                            className="col-start-1 row-start-1 size-5 self-center justify-self-end text-gray-500 sm:size-4"
+                        />
+                        </ListboxButton>
+
+                        <ListboxOptions
+                        transition
+                        className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg outline-1 outline-black/5 data-leave:transition data-leave:duration-100 data-leave:ease-in data-closed:data-leave:opacity-0 sm:text-sm"
+                        >
+                        {topics.map((topic) => (
+                            <ListboxOption
+                            key={topic.id}
+                            value={topic}
+                            className="group relative cursor-default py-2 pr-9 pl-3 text-gray-900 select-none data-focus:bg-indigo-600 data-focus:text-white data-focus:outline-hidden"
+                            >
+                            <span className="block truncate">{topic.des}</span>
+                            <span className="absolute inset-y-0 right-0 flex items-center pr-4 text-indigo-600 group-not-data-selected:hidden group-data-focus:text-white">
+                                <CheckIcon aria-hidden="true" className="size-5" />
+                            </span>
+                            </ListboxOption>
+                        ))}
+                        </ListboxOptions>
+                    </div>
+                </Listbox>
+            </fieldset>
+
+             <Link href="/diary" className="mt-8 inline-flex rounded-md bg-black px-4 py-2 text-sm text-white">
+                Back to Diary
+            </Link>
+        </main>
+    )
+}
--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -29,6 +29,30 @@ export default function WorkPage() {
         </a>
       </section>
 
+      <section id="project2">
+        <a
+          href={projectUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="mt-10 block w-fit"
+        >
+          <div className="card bg-base-100 w-96 shadow-sm transition hover:shadow-md">
+            <div className="card-body">
+              <h2 className="card-title">Auth System</h2>
+              <p>A secure full-stack authentication system built with Node.js, Express, and React (Vite). Users can register and log in with encrypted passwords using bcrypt, while JWT-based authentication protects private routes such as the dashboard. The application features a clean React interface and a lightweight JSON-based database, making it simple to run locally and easy to upgrade to a full database in the future.</p>
+            </div>
+            <figure>
+              <img
+                src="/Screenshot login.png"
+                alt="login_screenShot" />
+              <img
+                src="/Screenshot success.png"
+                alt="login success_screenShot" />
+            </figure>
+          </div>
+        </a>
+      </section>
+
       <Link href="/" className="mt-8 inline-flex rounded-md bg-black px-4 py-2 text-sm text-white">
         Back to Home
       </Link>
